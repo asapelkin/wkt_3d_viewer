fd19f48381d34f5d2bfb0a94e9fff0f326977432
asapelkin <asapelkin0x01@yandex.ru>
2018-12-05 00:01:37 +0300

    added geometrycollection support

--- public/webglearth.js
+++ public/webglearth.js
@@ -1,18 +1,15 @@
 var earth;
+var geoms = [];
 
 function initialize() {
     earth = new WE.map('earth_div');
     WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
         attribution: '© OpenStreetMap contributors'
     }).addTo(earth);
-
     earth.setView([48, 6], 5);
-    //setTimeout(foo, 10000);
-    //foo();
 }
 
 function foo() {
-
     pol0 = WE.polygon([[-69.2872321697549, 11.2956583022207],[-68.8453510163401, 10.0659632996469],[-70.8757657075917, 3.37227099999993],[-71.6665265170196, 3.37227099999993],[-69.2872321697549, 11.2956583022207]]  );
     pol0.addTo(earth);
     earth.update();
--- public/wktTools.js
+++ public/wktTools.js
@@ -4,35 +4,49 @@ function drawGeometry() {
     var wkt_string = document.getElementById('wktInput').value;
     var geometry = wellknown.parse(wkt_string.trim());
 
-    console.log(geometry);
+   // console.log(geometry);
+   // return;
+    geoms.forEach(function(item, i, arr) {
+        item.destroy();
+    });
 
+    var geomArray = geometryToPolygonsArray(geometry);
 
-     pol0 = WE.polygon(geometryToPolygonsArray(geometry));
-     pol0.addTo(earth);
-    // console.log(geometry);
+    geomArray.forEach(function(item, i, arr) {
+        var pol = WE.polygon(item, {
+            color: '#0412d6',
+            opacity: 1,
+            fillColor: '#0412d6',
+            fillOpacity: 0.1,
+            weight: 2
+        });
+        var gObj = pol.addTo(earth);
+        geoms.push(gObj);
+    });
+    if (geomArray.length > 0) {
+        var viewPoint = geomArray[0][0];
+        earth.setView(viewPoint, 5);
+    }
 }
-
-
 
 
 function geometryToPolygonsArray(geometry)
 {
-    if (geometry.type === "Polygon")
-    {
+    var res = [];
+    if (geometry.type === "Polygon") {
         var points_array = geometry.coordinates[0];
-        console.log(points_array);
-
-        points_array.forEach(function(item, i)
-        {
+        points_array.forEach(function(item, i){
             var buf = item[0];
             item[0] = item[1];
             item[1] = buf;
-            // item[1] = item[0];
-            // item[0] = buf;
         });
-        console.log(points_array);
-
-        return points_array;
-
+        res.push(points_array);
+        //return points_array;
+    } else if (geometry.type === "GeometryCollection") {
+        geometry.geometries.forEach(function(item, i, arr) {
+             res = res.concat(geometryToPolygonsArray(item));
+        });
     }
+   // console.log(res);
+    return res;
 }
