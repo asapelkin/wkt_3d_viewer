2ac8e9f7e155701c250ff6e8fe0c53df26a8a25b
asapelkin <asapelkin0x01@yandex.ru>
2018-12-06 18:06:25 +0300

    Create float frame

--- public/frames.js
+++ public/frames.js
@@ -1,27 +1,48 @@
+//Make the DIV element draggagle:
+dragElement(document.getElementById("InputWidget"));
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById("InputWidgetHeader")) {
+        /* if present, the header is where you move the DIV from:*/
+        document.getElementById("InputWidgetHeader").onmousedown = dragMouseDown;
+    } else {
+        /* otherwise, move the DIV from anywhere inside the DIV:*/
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
 
 $(function(){
-    var A = parseInt($('#input_div').width(), 10),
-        B = parseInt($('#earth_div').width(), 10),
-        Z = parseInt($('#Border').width(), 10),
-        minw = parseInt((A + B + Z) * 10 / 100, 10),
-        offset = $('#container').offset(),
-        splitter = function(event, ui){
-            var aw = parseInt(ui.position.left),
-                bw = A + B - aw;
-            //set widths and information...
-            $('#input_div').css({width : aw}).children();
-            $('#earth_div').css({width : bw}).children();
-        };
-    $('#Border').draggable({
-        axis : 'x',
-        containment : [
-            offset.left + minw,
-            offset.top,
-            offset.left + A + B - minw,
-            offset.top + $('#container').height()
-        ],
-        drag : splitter
-    });
-    //information...
-
+    $('.FloatWidget')
+        .resizable();
 });
